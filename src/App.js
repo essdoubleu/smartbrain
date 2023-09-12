61f46fb59900aeae06ae27539239a3b226256c33
Scott Windrem <scottwindrem@gmail.com>
2023-09-12 13:17:47 -0400

    updated fetch to correct addresses

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://smartbrain-backend-wbqa.onrender.com/image", {
             method: "put",
             headers: {
               "Content-Type": "application/json",
